f4217debefb7e50d39bccad082b0ed627382deb3
Gabriella-Ilieva <ilieva.gabriella@gmail.com>
2024-09-30 09:31:20 +0300

    Carousels

--- revolut.js
+++ revolut.js
@@ -94,3 +94,149 @@ backgrounds.forEach((bg, i) => {
             duration: 1});
 
 });
+
+const swiperInstances = [
+    new Swiper(".mySwiper", {  
+        loop: true,
+        slidesPerView: 3,
+        initialSlide: 2,
+        spaceBetween: 35,
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: 0,
+            scale: 0.9,
+            depth: 40,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 100000,
+            disableOnInteraction: false,
+    },
+    }),
+
+
+    new Swiper(".secondCarousel", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="carouselBtn ' + className + '"><span>' + buttons[index] + "</span></span>";
+            },
+        },
+        autoplay: {
+            delay: 8000,
+            disableOnInteraction: true,
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                let overlay = document.querySelector(".swiper-pagination-bullet-active > span");
+                overlay.style.setProperty("--widthOverlay", (1 - progress) * 100 + '%');
+            },
+
+            slideChange(swiper){
+                let overlayAll = document.querySelectorAll(".swiper-pagination-bullet > span");
+                
+                overlayAll.forEach(ol => {
+                    ol.style.setProperty("--widthOverlay", '0%');
+                });
+            }
+        }
+    }),
+
+
+    new Swiper(".thirdCarousel", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="carouselBtn ' + className + '"><span>' + buttonsThirdCarousel[index] + "</span></span>";
+            },
+        },
+        autoplay: {
+            delay: 8000,
+            stop: true,
+            disableOnInteraction: true,
+        },
+        on: {
+            // init(swiper) {
+            //     swiper.stop()
+            // },
+            autoplayTimeLeft(s, time, progress) {
+                let overlay2 = document.querySelector(".thirdCarousel .carouselBtn.swiper-pagination-bullet-active > span");
+                overlay2.style.setProperty("--widthOverlay", (1 - progress) * 100 + '%');
+            },
+
+            slideChange(swiper){
+                let overlayAll = document.querySelectorAll(".thirdCarousel .carouselBtn.swiper-pagination-bullet > span");
+                
+                overlayAll.forEach(ol => {
+                    ol.style.setProperty("--widthOverlay", '0%');
+                });
+
+                let currentSlide = swiper.slides[swiper.activeIndex].querySelector('video');
+                currentSlide.play();
+            }
+        }
+    }),
+
+
+
+    new Swiper(".fourthCarousel", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        effect: 'fade',
+        observer: true,
+        fadeEffect: {
+            crossFade: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="carouselBtn ' + className + '"><span>' + buttonsFourthCarousel[index] + "</span></span>";
+            },
+        },
+        autoplay: {
+            delay: 8000,
+            disableOnInteraction: true,
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                let overlay2 = document.querySelector(".fourthCarousel .carouselBtn.swiper-pagination-bullet-active > span");
+                overlay2.style.setProperty("--widthOverlay", (1 - progress) * 100 + '%');
+            },
+
+            slideChange(swiper){
+                let overlayAll = document.querySelectorAll(".fourthCarousel .carouselBtn.swiper-pagination-bullet > span");
+                
+                overlayAll.forEach(ol => {
+                    ol.style.setProperty("--widthOverlay", '0%');
+                });
+                
+                let currentSlide = swiper.slides[swiper.activeIndex].querySelector('video');
+                // currentSlide.currentTime = 0;
+                currentSlide.play();
+            }
+        }
+    }),
+]
