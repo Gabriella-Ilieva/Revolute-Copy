12d74b570738ae491eab278d3058ca4299909cf6
Gabriella-Ilieva <ilieva.gabriella@gmail.com>
2024-10-09 10:22:51 +0300

    Java script functions

--- revolut.js
+++ revolut.js
@@ -292,3 +292,20 @@ jQuery(document).ready(function() {
 
     
 })
+
+
+window.addEventListener('resize', () => {
+    location.reload();
+});
+
+document.addEventListener('wheel', function(e) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+    }
+  }, { passive: false });
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && (e.key === '+' || e.key === '-' || e.key === '0' || e.key === '=')) {
+      e.preventDefault();
+    }
+  });
